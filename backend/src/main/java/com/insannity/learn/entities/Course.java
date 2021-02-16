583d3152590ef577790032e4fa678665da12288b
insanniity <cleydson.cg@live.com>
2021-02-16 16:17:36 -0300

    [Backend] Offer

--- backend/src/main/java/com/insannity/learn/entities/Course.java
+++ backend/src/main/java/com/insannity/learn/entities/Course.java
@@ -1,11 +1,14 @@
 package com.insannity.learn.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +22,11 @@ public class Course  implements Serializable {
 	private String name;
 	private String imgUri;
 	private String imgGrayUri;
+	
+	@OneToMany(mappedBy = "course")
+	private List<Offer> offers = new ArrayList<>();	
+
+	
 
 	public Course() {
 	}
@@ -61,6 +69,10 @@ public class Course  implements Serializable {
 
 	public void setImgGrayUri(String imgGrayUri) {
 		this.imgGrayUri = imgGrayUri;
+	}
+	
+	public List<Offer> getOffers() {
+		return offers;
 	}
 
 	@Override
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,5 +13,7 @@ INSERT INTO tbl_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tbl_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tbl_user_role (user_id, role_id) VALUES (3, 3);
 
-
 INSERT INTO tbl_courses (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
+
+INSERT INTO tbl_offers (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2021-04-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-11-20T03:00:00Z', 1);
+INSERT INTO tbl_offers (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2021-05-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-12-20T03:00:00Z', 1);
